66b4d36022d83891d1a475252723da6321000cad
DJGuruji <nath93266@gmail.com>
2025-02-19 22:42:28 +0530

    password reset page loading added

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 const config = {
    
    
-   //   API_URL: "http://localhost:5000/",
-     API_URL:"https://virtualhospitalbackend.onrender.com/",
+     API_URL: "http://localhost:5000/",
+    //  API_URL:"https://virtualhospitalbackend.onrender.com/",
     
   };
   
